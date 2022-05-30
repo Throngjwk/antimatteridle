6be596f70f3a07bf06443cde750306b9879a6d96
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-05-30 12:01:15 -0300

    Update changelog-button.js

--- changelog-button.js
+++ changelog-button.js
@@ -1,3 +1,7 @@
 document.getElementById('versionnum1').onclick = () => {
     alert("What if antimatter dimeinsion retired used get comic sans ms?")
 };
+
+document.getElementById('versionnum2').onclick = () => {
+    alert("endless of fixed minor bugs")
+};
